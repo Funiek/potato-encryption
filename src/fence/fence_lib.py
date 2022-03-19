96467ab590467845262402cc51b0ec885843b66f
Maciej Fender <maciejf@int.pl>
2022-03-19 21:47:06 +0100

    Dodanie komentarzy

--- src/fence/fence_lib.py
+++ src/fence/fence_lib.py
@@ -1,43 +1,62 @@
 #!/usr/bin/python3
-def encrypt(value:str,key:str):
+"""
+Główna funckja do szyfrowania danych
+"""
+def encrypt(value: str, key: str):
+    # Pobranie tablicy generowanej na podstawie klucza zawierającej
+    #  w każdym polu kolejny indeks poziomu
+    a = getHelperArray(value, key)
 
-	a = getHelperArray(value,key)
+    ret = []
+    # Iteracja po poziomach
+    for ii in range(1, int(key) + 1):
+        # iteracja po tekście jawnym
+        for i, v in enumerate(value):
+            # gdy poziom jest równy z wartością przypisaną w tablicy pomocniczej
+            #  dodawany jest znak do szyfrogramu
+            if a[i] == ii:
+                ret.append(v)
+    # zapisanie wyniku w postaci ciągu znaków z tablicy
+    return ''.join(ret)
 
-	ret = []
-	for ii in range(1,int(key)+1):
-		for i,v in enumerate(value):
-			if a[i] == ii:
-				ret.append(v)
-	return ''.join(ret)
 
-def decrypt(value:str,key:str):
-	a = getHelperArray(value,key)
-	message = ['']*len(a)
-	buffor_pointer = 0
+def decrypt(value: str, key: str):
+    # Pobranie tablicy pomocniczej zawierającej indeksy poziomów dla liter
+    #  tekstu szyfrowanego na podstawie klucza
+    a = getHelperArray(value, key)
+    # ustawienie początkowych wartości zmiennym
+    message = [''] * len(a)
+    bufor_pointer = 0
 
-	for k in range(1,int(key)+1):
-		for i in range(0,len(value)):
-			if k == a[i]:
-				message[i] = value[buffor_pointer]
-				buffor_pointer += 1
-				
-	return ''.join(message)
+    # iteracja po wartościach poziomów szyfru na podstawie klucza
+    for k in range(1, int(key) + 1):
+        # Iteracja po literach szyfrogramu
+        for i in range(0, len(value)):
+            # jeżeli wartości poziomów się zgadzają zapisywana jest litera w wiadomosci
+            if k == a[i]:
+                message[i] = value[bufor_pointer]
+                bufor_pointer += 1
+    # przekształcenie wyniku do postaci końcowej
+    return ''.join(message)
 
-def getHelperArray(value:str,key:str):
-	a=[0]*len(value)
-	zwrot = -1
-	for i,v in enumerate(value):
-		if i == 0:
-			a[i] = 1
-			continue
-		if zwrot == -1:
-			a[i] = a[i-1]+1
-		else:
-			a[i] = a[i-1]-1
-		if a[i] == int(key) or a[i] == 1:
-			zwrot = (-1) * zwrot
-	return a
+
+def getHelperArray(value: str, key: str):
+    a = [0] * len(value)
+    zwrot = -1
+    for i, v in enumerate(value):
+        if i == 0:
+            a[i] = 1
+            continue
+        if zwrot == -1:
+            a[i] = a[i - 1] + 1
+        else:
+            a[i] = a[i - 1] - 1
+        if a[i] == int(key) or a[i] == 1:
+            zwrot = (-1) * zwrot
+    return a
+
 
 if __name__ == "__main__":
-	import sys
-	print(decrypt(encrypt(sys.argv[1],sys.argv[2]),sys.argv[2]))
+    import sys
+
+    print(decrypt(encrypt(sys.argv[1], sys.argv[2]), sys.argv[2]))
--- src/matrix2d/matrixCipher.py
+++ src/matrix2d/matrixCipher.py
@@ -1,45 +1,65 @@
 #!/usr/bin/python3
 
+"""
+Metoda do enkrypcji
+"""
 def encrypt(message: str, key: str):
+    # Pobranie tablicy przestawień
     k_array = getArrayKey(key)
+    # Usunięcie znaków białych
     message = message.replace(" ", '')
     ret = []
-
+    # Iteracja po macierzy przestawień
     for y in k_array:
         index = 1
+        # Iteracja po tekście jawnym przesuwając się o długość klucza
         while index + y - 2 < len(message):
+            # dodanie znaku do wyniku
             ret.append(message[index + y - 2])
             index += len(k_array)
+        # Oddzielenie sekcji spacjami
         ret.append(" ")
+    # Przygotowanie końcowego wyniku
     return ''.join(ret[:-1])
 
 
+"""
+Metoda do dekrypcji
+"""
 def decrypt(message: str, key: str):
-    print(key)
+    # Usunięcie znaków białych
     message = message.replace(" ", '')
+    # Pobranie tablicy przestawień
     k_array = getArrayKey(key)
 
     result = [''] * len(message)
     i = 0
+    # Iteracja po tablicy przestawień
     for y in k_array:
         index = 1
+        # Iteracja po szyfrogramie iterując się po znakach oddalonych od siebie o długość klucza
         while index + y - 2 < len(message):
+            # Przypisanie wyniku
             result[index + y - 2] = message[i]
+            # Zwiększenie indeksów
             index += len(k_array)
             i += 1
-
+    # Przygotowanie finalnej wersji wyniku
     return ''.join(result)
 
-
+"""
+Opracowanie tablicy na podstawie klucza, która będzie wzorem przestawień znaków.
+"""
 def getArrayKey(key: str):
-    arr = list(range(1, int(len(key)) + 1))
-    arr = list(zip(key, arr))
+    # Przygotowanie na podstawie znaków listy tupli zawierających litere i indeks
+    arr = list(zip(key, list(range(1, int(len(key)) + 1))))
+    # Posortowanie ciągu po znaku
     arr.sort()
+    # Usunięcie znaku pozostawiając nr przestawień
     arr = [i[1] for i in arr]
     return arr
 
 
 if __name__ == "__main__":
     import sys
-
     print(decrypt(encrypt("HERE IS A SECRET MESSAGE ENCIPHERED BY TRANSPOSITION", "CONVENIENCE"), "CONVENIENCE"))
